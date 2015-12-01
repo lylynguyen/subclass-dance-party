459f2f2a96d13ab3d9a4de6adf4b1e8613bee751
Fred Zirdung <fred@hackreactor.com>
2015-11-30 16:47:42 -0800

    Fix spec

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -23,7 +23,7 @@ describe("blinkyDancer", function() {
     it("should call step at least once per second", function() {
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
