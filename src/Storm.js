a65ac0b4aaeb8db41999e87d68595ac1081cfe2e
lylynguyen <lylynguyen812@gmail.com>
2015-12-01 20:12:19 -0800

    Slowed down asteroid step

--- src/Storm.js
+++ src/Storm.js
@@ -1,17 +1,24 @@
 var Storm = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="asteroid"></span>');
+
 };
 
 Storm.prototype = Object.create(Dancer.prototype);
 Storm.prototype.constructor = Storm;
 
 Storm.prototype.step = function() {
-  if (this.move) {
-    this.top =  $("body").height() * Math.random();
-    this.left =  $("body").width() * Math.random();
-    this.setPosition();
-  }
-    Dancer.prototype.step.call(this);
+    setTimeout(function() {
+      if (this.move) {
+        this.top =  $("body").height() * Math.random();
+        this.left =  $("body").width() * Math.random();
+        this.height = 40;
+        this.width = 40;
+        this.setPosition();  
+      }
+        Dancer.prototype.step.call(this);
+      }.bind(this), 300);
+
 };
 
 
