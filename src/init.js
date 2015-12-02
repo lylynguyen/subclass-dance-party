9c8be169aa50db7c1c4fa7244fb12788effb4cee
lylynguyen <lylynguyen812@gmail.com>
2015-12-01 16:27:50 -0800

    add equal width steps when clicking line up

--- src/init.js
+++ src/init.js
@@ -25,10 +25,15 @@ $(document).ready(function() {
   });
 
   $('.lineUp').on('click', function(e) {
+    var totalWidth = $('body').width()
+    var sep = totalWidth/window.dancers.length
     e.preventDefault()
     for(var i = 0; i < window.dancers.length; i++){
       var currentDancer = window.dancers[i];
+      currentDancer.left = i*sep;
+      currentDancer.step()
       currentDancer.move = !currentDancer.move;
+
     }
     $(".pilot").animate({
         top: "400px"
